a3bd97d22b8ec08b39bb9de9a7cab3d2f0f3acb2
parker_mansfield <pmansfield@uvu.edu>
2019-08-23 22:25:30 -0600

    Implemented Hello class with say function

---
+++ src/main/java/uvu/cs/cs3250/Hello.java
@@ -0,0 +1,12 @@
+package uvu.cs.cs3250;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println(say());
+	}
+	
+	public static String say() {
+		return "Hello, Matt";
+	}
+}
