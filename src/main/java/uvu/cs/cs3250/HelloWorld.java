56e62524cddf0f09be67b79d4f49e40ae6b86abc
evsterfy <evanglassford@live.com>
2019-08-25 13:40:55 -0600

    first

--- src/main/java/uvu/cs/cs3250/HelloWorld.java
+++ src/main/java/uvu/cs/cs3250/HelloWorld.java
@@ -3,7 +3,10 @@ package uvu.cs.cs3250;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Matt");
+		System.out.println(say());
 	}
 
+	public static String say() {
+		return "Hello, Matt";
+	}
 }
