479097192d254fa291933e08cf6868ddbea04d3f
15rtrujillo <ryantrujillo@comcast.net>
2019-08-25 13:13:27 -0600

    HelloMatt assignment submission

---
+++ src/main/java/uvu/cs/cs3250/Hello.java
@@ -0,0 +1,10 @@
+package uvu.cs.cs3250;
+
+public class Hello {
+	public static String say() {
+		return "Hello, Matt!";
+	}
+	public static void main(String[] args) {
+		System.out.println(say());
+	}
+}
