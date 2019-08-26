53303eac7567a602877242a78f2bd1407ef7bece
Unknown <skyesladeut@outlook.com>
2019-08-25 22:32:45 -0600

    Adding Hello_Matt Stuff

---
+++ src/main/java/uvu/cs/cs3250/HelloMatt.java
@@ -0,0 +1,9 @@
+package uvu.cs.cs3250.assignments;
+
+public class HelloMatt {
+	public String say() {
+		//will cause failure because it is not "Hi Matt"
+		return "Hello, Matt";
+		//return "Hi Matt" for no failure trace
+	}
+}
