d20fe05a78032125da0bbd8f9730b62681cb6b1b
zachjareth@gmail.com <zachjareth@gmail.com>
2019-08-26 17:25:54 -0600

    Added Say Method
    Added say method to the HelloWorld.java file - commented out the driver code

--- src/main/java/uvu/cs/cs3250/HelloWorld.java
+++ src/main/java/uvu/cs/cs3250/HelloWorld.java
@@ -2,8 +2,12 @@ package uvu.cs.cs3250;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	static String say() {
+		return "Hi Matt";
+	}
+	
+	/*public static void main(String[] args) {  //previous code
 		System.out.println("Hello Matt");
 	}
-
+	*/
 }
