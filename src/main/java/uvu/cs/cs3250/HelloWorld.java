30f10f77f584f7f279a167f3c5cf8ac4cde14543
McCrakken <10561310@my.uvu.edu>
2019-08-26 18:08:07 -0600

    Changed HelloWorldTest and HelloWorld to test for "Hi Matt" string

--- src/main/java/uvu/cs/cs3250/HelloWorld.java
+++ src/main/java/uvu/cs/cs3250/HelloWorld.java
@@ -1,7 +1,13 @@
+//Packages are all lowercase
 package uvu.cs.cs3250;
 
+//Class names are written with a capital camel-case
 public class HelloWorld {
 
+	//function and identifiers start with lower and go camel-case
+	public String say() {
+		return "Hi Matt";
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello, McKay Larsen");
 	}
